e176533eb4e324d06672d5c96da22e510944fea6
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-10-04 18:53:01 +0530

    Upto batches feature
    Signed-off-by: Krishna Nand Patel <imkrishna8737@gmail.com>

--- pages/teachers/[id].jsx
+++ pages/teachers/[id].jsx
@@ -1,6 +1,23 @@
+import teachers from './teacher.json'
+export const getStaticPaths=async()=>{
+    const paths=teachers.map(teacher=>{
+        return {
+            params: {id : teacher.id.toString()}
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+export const getStaticProps=async(context)=>{
+    const id=context.params.id;
+    const data=teachers.id;
+    console.log(data)
+}
 const Teacher=()=>{
     return (
-        <h1>Teacher 1</h1>
+        <h1>Teacher </h1>
     )
     }
     export default Teacher;

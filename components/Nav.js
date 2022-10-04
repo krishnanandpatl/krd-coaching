76151615e34b4ec0e9fccd326048dbba80d9817b
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-10-04 14:45:04 +0530

    Some more feature added
    Signed-off-by: Krishna Nand Patel <imkrishna8737@gmail.com>

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,12 @@
 import styles from '../styles/Nav.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Nav=()=>{
     return (
     <>
     <div className={styles.navbar}>
-        <Link href='/'><a className={styles.title}>KRD</a></Link>
+        <Link href='/'><Image src='/KRD1.png' height='48px' width='60px'></Image></Link>
         <div className={styles.navLeft}>
             <a className={styles.batches}>Batches</a>
             <Link href='/teachers'><a className={styles.teachers}>Teachers</a></Link>
